97728abd38e8b2fdb7ebb1de63563a7c763de3da
vscalcione <vincenzo99.scalcione@gmail.com>
2021-07-07 11:16:16 +0200

    Initial commit - Initialize Springboot project

---
+++ crudapp-backend/src/test/java/it/vscalcione/springboot/crudappbackend/CrudappBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package it.vscalcione.springboot.crudappbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrudappBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
