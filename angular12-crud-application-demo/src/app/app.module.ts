11684784922f9cf3469cc0cd06e72ca156ea0c6c
DIR\vincenzo.scalcione <vincenzo.scalcione@accenture.com>
2021-07-07 12:25:16 +0200

    Angular project updated - added employee components, employee service and employee model

--- angular12-crud-application-demo/src/app/app.module.ts
+++ angular12-crud-application-demo/src/app/app.module.ts
@@ -4,10 +4,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { CreateEmployeeComponent } from './employee/create-employee/create-employee.component';
+import { EmployeeDetailsComponent } from './employee/employee-details/employee-details.component';
+import { EmployeeListComponent } from './employee/employee-list/employee-list.component';
+import { UpdateEmployeeComponent } from './employee/update-employee/update-employee.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CreateEmployeeComponent,
+    EmployeeDetailsComponent,
+    EmployeeListComponent,
+    UpdateEmployeeComponent
   ],
   imports: [
     BrowserModule,
