26b1c4ec22c1e11bbe17f8bc9c7ea74493b03912
DIR\vincenzo.scalcione <vincenzo.scalcione@accenture.com>
2021-07-07 12:37:10 +0200

    Angular frontend project updated - update employee.service.ts, app.module.ts, app-routing.module.ts files

--- angular12-crud-application-demo/src/app/app-routing.module.ts
+++ angular12-crud-application-demo/src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+import { CreateEmployeeComponent } from './employee/components/create-employee/create-employee.component';
+import { EmployeeDetailsComponent } from './employee/components/employee-details/employee-details.component';
+import { EmployeeListComponent } from './employee/components/employee-list/employee-list.component';
+import { UpdateEmployeeComponent } from './employee/components/update-employee/update-employee.component';
+
+const routes: Routes = [
+  { path: 'employees', component: EmployeeListComponent },
+  { path: 'createEmployee', component: CreateEmployeeComponent },
+  { path: 'updateEmployee/:id', component: UpdateEmployeeComponent },
+  { path: 'employeeDetails/:id', component: EmployeeDetailsComponent },
+  { path: '', redirectTo: 'employees', pathMatch: 'full' }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- angular12-crud-application-demo/src/app/app.module.ts
+++ angular12-crud-application-demo/src/app/app.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HttpClientModule } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
+
 import { CreateEmployeeComponent } from './employee/create-employee/create-employee.component';
 import { EmployeeDetailsComponent } from './employee/employee-details/employee-details.component';
 import { EmployeeListComponent } from './employee/employee-list/employee-list.component';
@@ -20,7 +22,8 @@ import { UpdateEmployeeComponent } from './employee/update-employee/update-emplo
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
